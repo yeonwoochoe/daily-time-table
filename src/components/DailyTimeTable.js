43f7b10fa436a2a811c98ed504a31b890ddb11ed
YEONWOO <89890240+yeonwoochoe@users.noreply.github.com>
2022-01-22 20:24:29 +0900

    [feat] components 🆕 Daily Time Table chart.js 이용하여 간단히 만들기 완성

---
+++ src/components/DailyTimeTable.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { Pie } from 'react-chartjs-2'
+
+
+export default function DailyTimeTable() {
+
+  return (
+
+
+<Pie
+  data={{
+    labels: [
+      '23-08    꿈나라💤💤💤',
+      '08-09    기상 및 공부준비😵‍💫 ',
+      '09-13    리액트 강의',
+      '13-14    운동 gogo💪🏻',
+      '🌈REST🌈         ',
+      '15시~19시 🔥복습🔥',
+      '19시~21시 프로젝트👩🏻‍💻👩🏻‍💻',
+      '🌈REST🌈'
+    ],
+
+    datasets: [{
+      label: 'My First Dataset',
+      data: [400, 100, 100, 100, 100, 100, 100,100],
+      borderWidth: 2,
+      fill: true,
+      backgroundColor: [
+        'rgba(30, 76, 148,1.0)',
+      'rgba(130, 100, 230,1.0)',
+      'rgba(148, 200, 222,1.0)',
+      'rgba(116, 185, 255,1.0)',
+      'rgba(0, 184, 148,1.0)',
+      'rgba(255, 234, 167,1.0)',
+      'rgba(255, 118, 117,1.0)',
+      'rgba(0, 184, 148,1.0)'
+    ]
+
+    }]
+  }}
+
+  options={{ maintainAspectRatio: true,
+ 
+    layout: {
+      padding: {
+          left: 0,
+          right: 70,
+          top: 0,
+          bottom: 50
+      }
+  },
+    hoverBorderWidth: 20,
+    responsive: true,
+    legend: {
+      position: "right",
+      display: true,
+      labels: {
+        padding:30,
+        fontColor: 'rgb(255, 99, 132)',
+        fontFamily:'S-CoreDream-5Medium',
+        fontWeight: '400',
+        fontSize:30,
+        boxWidth:14,
+        usePointStyle:true
+    }
+    }
+
+
+  
+  
+  }
+}
+
+
+
+/>
+ 
+  )
+}
+
+
---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Title() {
+  return (
+  <div className="main_title">
+    <h1>Yeonwoo's <span style={{color:"#f94"}}>  Daily Time Table</span></h1>
+    <p>🔥취업 전까지는 꼬옥! 계획표대로 살기!!🔥 </p>
+  </div>
+  );
+}
+
+export default Title;
